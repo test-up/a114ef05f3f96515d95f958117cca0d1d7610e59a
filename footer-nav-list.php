6c05aa780c3296487b8cc4cffc692a259b59e9db
菊池康明 <d130@kikuchiyasuakinoMacBook-Pro.local>
2020-03-09 17:39:19 +0900

    sp完成

---
+++ footer-nav-list.php
@@ -0,0 +1,201 @@
+<ul class="footer-nav-list" id="footer-nav-list">
+        <li class="footer-nav-listitem" id="listitem-001">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/001.svg" alt="001"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-002">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/002.svg" alt="002"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+
+
+
+        <li class="footer-nav-listitem" id="listitem-003">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/003.svg" alt="003"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-004">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/004.svg" alt="004"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem" id="listitem-005">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/005.svg" alt="005"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-006">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/006.svg" alt="006"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-007">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/007.svg" alt="007"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-008">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/008.svg" alt="008"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem" id="listitem-009">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/009.svg" alt="009"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem" id="listitem-010">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/010.svg" alt="010"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-011">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/011.svg" alt="011"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem" id="listitem-012">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/012.svg" alt="012"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-013">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/013.svg" alt="013"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem"  id="listitem-014">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/014.svg" alt="014"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-015">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/015.svg" alt="015"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+        <li class="footer-nav-listitem" id="listitem-016">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/016.svg" alt="016"></p>
+          <a href="001.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/business-administration/yadviga/main.jpg" alt="Kandratsenkava Yadviga">
+            </figure>
+            <p class="footer-nav-listitem-p1">経営学部経営学科</p>
+            <p class="footer-nav-listitem-p2">Kandratsenkava Yadviga</p>
+            <p class="footer-nav-listitem-p3 pink">アニメに惹かれて東欧から日本へ。寮友と過ごす日々で、世界は広がっていく。</p>
+          </a>
+        </li>
+
+        <li class="footer-nav-listitem" id="listitem-017">
+          <p class="footer-nav-listitem-number"><img src="img/people/number/017.svg" alt="017"></p>
+          <a href="002.html">
+            <figure class="footer-nav-listitem-photo">
+              <img src="img/people/economics/morihara/main.jpg" alt="森原 佳大">
+            </figure>
+            <p class="footer-nav-listitem-p1">経済学部経済学科</p>
+            <p class="footer-nav-listitem-p2">森原 佳大</p>
+            <p class="footer-nav-listitem-p3 blue">日常生活のサポートから思い出づくりまで「105」が目指す留学生支援のかたち。</p>
+          </a>
+        </li>
+      </ul>
